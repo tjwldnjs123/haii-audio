a67e667df2137764646a1203def19ad3f41262e9
tjwldnjs123 <tjwldnjs12345@naver.com>
2022-10-11 23:55:57 +0900

    [7팀 서지원] Modify: nav폴더 구조 변경

--- src/pages/nav/Nav.js
+++ src/pages/nav/Nav.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faRecordVinyl } from "@fortawesome/free-solid-svg-icons";
