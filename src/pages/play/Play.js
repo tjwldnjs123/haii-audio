f5d86741f01d21133426dce82e0404c7cd886eab
lkw1996 <lkw3703@gmail.com>
2022-10-12 16:29:12 +0900

    Add : wavesurfer.js 라이브러리 추가

--- src/pages/play/Play.js
+++ src/pages/play/Play.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useCallback, useRef } from "react";
+import { WaveSurfer, WaveForm } from "wavesurfer-react";
 
 const Play = () => {
   return <div>재생</div>;
