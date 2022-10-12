5e677fe70c5fae380de7a7e0b660e3c5a83361d0
zionii <tjwldnjs12345@naver.com>
2022-10-12 13:46:49 +0900

    [7팀 서지원] Add: 녹음기능 추가후 url state저장 (#6)

--- src/Router.js
+++ src/Router.js
@@ -1,16 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Nav from "./pages/nav/Nav";
 import Play from "./pages/play/Play";
 import Record from "./pages/record/Record";
 
 function Router() {
+  const [url, setUrl] = useState("");
+  const [file, setFile] = useState([]);
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Nav />} />
-        <Route path="/record" element={<Record />} />
-        <Route path="/play" element={<Play />} />
+        <Route path="/" element={<Nav file={file} />} />
+        <Route
+          path="/record"
+          element={<Record setUrl={setUrl} setFile={setFile} />}
+        />
+        <Route path="/play" element={<Play url={url} />} />
       </Routes>
     </BrowserRouter>
   );
