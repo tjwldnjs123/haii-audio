196c81a978c59d1f2088040146610f81f8a42f0f
이기완 <51150501+lkw1996@users.noreply.github.com>
2022-10-12 16:43:46 +0900

    [7팀 이기완] Add : 오디오 재생 및 파형 구현 (#9)
    * Add : wavesurfer.js 라이브러리 추가
    
    * Add : 재생 기능 및 오디오 파형 구현
    
    * Design : 오디오 태그 스타일링

--- src/pages/play/Play.js
+++ src/pages/play/Play.js
@@ -1,7 +1,62 @@
-import React from "react";
+import React, { useCallback, useRef } from "react";
+import { WaveSurfer, WaveForm } from "wavesurfer-react";
+import styled from "styled-components";
 
-const Play = () => {
-  return <div>재생</div>;
+const Play = ({ url }) => {
+  const wavesurferRef = useRef();
+
+  const handleWave = useCallback((waveSurfer) => {
+    wavesurferRef.current = waveSurfer;
+
+    if (wavesurferRef.current) {
+      wavesurferRef.current.load(url);
+    }
+  }, []);
+
+  const play = useCallback(() => {
+    wavesurferRef.current.play();
+  }, []);
+
+  const pause = useCallback(() => {
+    wavesurferRef.current.pause();
+  }, []);
+
+  return (
+    <WaveContainer>
+      <WaveSurfer onMount={handleWave}>
+        <WaveForm
+          id="waveform"
+          waveColor={"white"}
+          barHeight={2}
+          barWidth={2}
+          progressColor={"blue"}
+          responsive={true}
+          interact={false}
+        />
+      </WaveSurfer>
+      <audio
+        onPlay={play}
+        onPause={pause}
+        src={url}
+        controls
+        controlsList="noplaybackrate"
+      ></audio>
+      ;
+    </WaveContainer>
+  );
 };
+
+const WaveContainer = styled.div`
+  background-color: black;
+  text-align: center;
+
+  audio::-webkit-media-controls-timeline {
+    display: none;
+  }
+
+  audio::-webkit-media-controls-volume-slider {
+    margin-left: 500px;
+  }
+`;
 
 export default Play;
