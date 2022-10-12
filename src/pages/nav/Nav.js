5f12a8413d815c5a87199d5d1935cfd323f2ce45
kimchoongman <103557910+kimchoongman@users.noreply.github.com>
2022-10-12 13:07:02 +0900

    [7팀김충만] Feature/record (#5)
    * Add: record UI 작업 시작
    
    * Add: 시간 추가
    
    * Add: react icons 설치

--- src/pages/nav/Nav.js
+++ src/pages/nav/Nav.js
@@ -1,27 +1,26 @@
-import React, { useState } from "react";
-import styled from "styled-components";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faRecordVinyl } from "@fortawesome/free-solid-svg-icons";
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { VscRecord } from 'react-icons/vsc';
 
 const Nav = () => {
   return (
     <NavContainer>
-      <div className="nav-bar-container">
-        <p className="nav-header">모든 녹음 항목</p>
-        <ul className="nav-list-container">
-          <li className="nav-list">
-            <div className="address">역삼동 736-38</div>
-            <div className="time-container">
+      <div className='nav-bar-container'>
+        <p className='nav-header'>모든 녹음 항목</p>
+        <ul className='nav-list-container'>
+          <li className='nav-list'>
+            <div className='address'>역삼동 736-38</div>
+            <div className='time-container'>
               <p>오후 8:00</p>
               <p>00:04</p>
             </div>
           </li>
         </ul>
-        <div className="nav-footer">
-          <FontAwesomeIcon className="record" icon={faRecordVinyl} />
+        <div className='nav-footer'>
+          <VscRecord className='record' />
         </div>
       </div>
-      <div className="main">
+      <div className='main'>
         <p>음성 메모를 시작하려면 녹음 버튼을 클릭하십시오.</p>
       </div>
     </NavContainer>
@@ -59,7 +58,6 @@ const NavContainer = styled.div`
       color: #fff;
 
       .nav-list {
-        height: 50px;
         padding: 5%;
         border-bottom: 1px solid ${({ theme }) => theme.regularGreen};
 
