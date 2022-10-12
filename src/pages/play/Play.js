b9ea50066b27addbd75ec945377e92b9980237d8
lkw1996 <lkw3703@gmail.com>
2022-10-12 16:37:03 +0900

    Add : 재생 기능 및 오디오 파형 구현

--- src/pages/play/Play.js
+++ src/pages/play/Play.js
@@ -1,8 +1,48 @@
 import React, { useCallback, useRef } from "react";
 import { WaveSurfer, WaveForm } from "wavesurfer-react";
 
-const Play = () => {
-  return <div>재생</div>;
+const Play = ({ url }) => {
+  const wavesurferRef = useRef();
+
+  const handleWave = useCallback((waveSurfer) => {
+    wavesurferRef.current = waveSurfer;
+
+    if (wavesurferRef.current) {
+      wavesurferRef.current.load(url);
+    }
+  }, []);
+
+  const play = useCallback(() => {
+    wavesurferRef.current.play();
+  }, []);
+
+  const pause = useCallback(() => {
+    wavesurferRef.current.pause();
+  }, []);
+
+  return (
+    <>
+      <WaveSurfer onMount={handleWave}>
+        <WaveForm
+          id="waveform"
+          waveColor={"white"}
+          barHeight={2}
+          barWidth={2}
+          progressColor={"blue"}
+          responsive={true}
+          interact={false}
+        />
+      </WaveSurfer>
+      <audio
+        onPlay={play}
+        onPause={pause}
+        src={url}
+        controls
+        controlsList="noplaybackrate"
+      ></audio>
+      ;
+    </>
+  );
 };
 
 export default Play;
