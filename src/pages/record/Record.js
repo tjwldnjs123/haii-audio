f8182b278efe26ffaa303c9dfb921e9376630aea
kimchoongman <103557910+kimchoongman@users.noreply.github.com>
2022-10-13 22:18:36 +0900

    [7팀 김충만] Fix/timer (#19)
    * Add: 시간 지정 녹음 기능 구현
    
    * Modify: 배경색 변경
    
    * Modify: ui 변경
    
    * Modify: 버튼 스타일 수정
    
    * Merge branch 'main' into fix/timer
    
    * Modify: record  UI 수정
    
    * Modify: play 페이지 뒤로가기 버튼 추가
    
    * Add: option 값으로 녹음 시간 설정 구현
    
    * Add: select css 추가

--- src/pages/record/Record.js
+++ src/pages/record/Record.js
@@ -5,7 +5,7 @@ import waveSound from "../../assets/wave-sound.png";
 import { BsFillRecordFill } from "react-icons/bs";
 import { IoIosSquare } from "react-icons/io";
 
-const Record = ({ setFile }) => {
+const Record = () => {
   const [stream, setStream] = useState();
   const [media, setMedia] = useState();
   const [onRec, setOnRec] = useState(true);
@@ -13,6 +13,7 @@ const Record = ({ setFile }) => {
   const [analyser, setAnalyser] = useState();
   const [audioUrl, setAudioUrl] = useState();
   const [timer, setTimer] = useState("00:00.00");
+  const [seconds, setSeconds] = useState("");
 
   const navigate = useNavigate();
 
@@ -37,7 +38,7 @@ const Record = ({ setFile }) => {
       makeSound(stream);
 
       analyser.onaudioprocess = function (e) {
-        if (e.playbackTime > 10) {
+        if (e.playbackTime > seconds) {
           stream.getAudioTracks().forEach(function (track) {
             track.stop();
           });
@@ -86,19 +87,32 @@ const Record = ({ setFile }) => {
 
     sessionStorage.setItem("file", sound.name);
     navigate("/");
-  }, [audioUrl]);
+  }, [audioUrl, navigate]);
   return (
     <StyledRecord>
-      <section className="wave-form">Haii-audio</section>
-      <section className="sound-wave">
-        <img alt="파형" src={waveSound} />
+      <section className='wave-form'>Haii-audio</section>
+      <section className='sound-wave'>
+        <img alt='파형' src={waveSound} />
       </section>
-      <section className="record-time">{timer}</section>
-      <footer className="record-btn-box">
-        <div className="record-btn" onClick={onRec ? onRecAudio : offRecAudio}>
+      <section className='record-time'>{timer}</section>
+      <select
+        aria-label='record-time-control'
+        className='time-control'
+        onChange={(e) => setSeconds(Number(e.target.value))}
+      >
+        <option>녹음 시간 설정</option>
+        <option value='10'>10초</option>
+        <option value='20'>20초</option>
+        <option value='30'>30초</option>
+        <option value='40'>40초</option>
+        <option value='50'>50초</option>
+        <option value='60'>60초</option>
+      </select>
+      <footer className='record-btn-box'>
+        <div className='record-btn' onClick={onRec ? onRecAudio : offRecAudio}>
           {onRec ? <StyledCircle /> : <StyledSquare />}
         </div>
-        <div className="complete-btn" onClick={onSubmitAudioFile}>
+        <div className='complete-btn' onClick={onSubmitAudioFile}>
           완료
         </div>
       </footer>
@@ -110,14 +124,14 @@ const StyledSquare = styled(IoIosSquare)`
   padding: 5px;
   border: 1px solid #fff;
   border-radius: 50%;
-  font-size: xx-large;
+  font-size: 50px;
 `;
 
 const StyledCircle = styled(BsFillRecordFill)`
   padding: 5px;
   border: 1px solid #fff;
   border-radius: 50%;
-  font-size: xx-large;
+  font-size: 50px;
 `;
 
 const StyledRecord = styled.section`
@@ -173,12 +187,28 @@ const StyledRecord = styled.section`
     color: #fff;
   }
 
+  .time-control {
+    width: 160px;
+    padding: 12px 13px;
+    border: 1px solid #fff;
+    border-radius: 10px;
+    font-weight: 500;
+    font-size: 15px;
+    line-height: 16px;
+
+    &:focus {
+      border: 1px solid silver;
+      border-radius: 10px;
+      outline: 2px solid silver;
+    }
+  }
+
   .record-btn-box {
-    width: 200px;
+    width: 300px;
     margin: 20px 0;
     display: flex;
-    justify-content: space-between;
-    color: #000;
+    align-items: center;
+    justify-content: space-around;
 
     .record-btn {
       font-size: xx-large;
@@ -190,10 +220,11 @@ const StyledRecord = styled.section`
     }
 
     .complete-btn {
-      padding: 5px 30px;
-      border: 1.5px solid #fff;
+      padding: 10px 30px;
+      border: 1px solid #fff;
       border-radius: 30px;
       font-weight: 600;
+      font-size: larger;
       color: #fff;
 
       &:hover {
