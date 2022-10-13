4f4d16adbfd675c8e801533955808e995c27c00e
tjwldnjs123 <tjwldnjs12345@naver.com>
2022-10-13 16:49:08 +0900

    Remove: 주석삭제

--- src/pages/record/Record.js
+++ src/pages/record/Record.js
@@ -1,9 +1,9 @@
-import React, { useCallback, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import styled from 'styled-components';
-import waveSound from '../../assets/wave-sound.png';
-import { BsFillRecordFill } from 'react-icons/bs';
-import { IoIosSquare } from 'react-icons/io';
+import React, { useCallback, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import waveSound from "../../assets/wave-sound.png";
+import { BsFillRecordFill } from "react-icons/bs";
+import { IoIosSquare } from "react-icons/io";
 
 const Record = ({ setFile }) => {
   const [stream, setStream] = useState();
@@ -12,28 +12,23 @@ const Record = ({ setFile }) => {
   const [source, setSource] = useState();
   const [analyser, setAnalyser] = useState();
   const [audioUrl, setAudioUrl] = useState();
-  const [timer, setTimer] = useState('00:00.00');
+  const [timer, setTimer] = useState("00:00.00");
 
   const navigate = useNavigate();
 
   const onRecAudio = () => {
-    // 음원정보를 담은 노드를 생성하거나 음원을 실행또는 디코딩 시키는 일을 한다
     const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-
-    // 자바스크립트를 통해 음원의 진행상태에 직접접근에 사용된다.
     const analyser = audioCtx.createScriptProcessor(0, 1, 1);
     setAnalyser(analyser);
 
     function makeSound(stream) {
-      // 내 컴퓨터의 마이크나 다른 소스를 통해 발생한 오디오 스트림의 정보를 보여준다.
       const source = audioCtx.createMediaStreamSource(stream);
       setSource(source);
 
-      // AudioBufferSourceNode 연결
       source.connect(analyser);
       analyser.connect(audioCtx.destination);
     }
-    // 마이크 사용 권한 획득 후 녹음 시작
+
     navigator.mediaDevices.getUserMedia({ audio: true }).then((stream) => {
       const mediaRecorder = new MediaRecorder(stream);
       mediaRecorder.start();
@@ -42,13 +37,11 @@ const Record = ({ setFile }) => {
       makeSound(stream);
 
       analyser.onaudioprocess = function (e) {
-        // 3분(180초) 지나면 자동으로 음성 저장 및 녹음 중지
         if (e.playbackTime > 10) {
           stream.getAudioTracks().forEach(function (track) {
             track.stop();
           });
           mediaRecorder.stop();
-          // 메서드가 호출 된 노드 연결 해제
           analyser.disconnect();
           audioCtx.createMediaStreamSource(stream).disconnect();
 
@@ -56,7 +49,7 @@ const Record = ({ setFile }) => {
             setAudioUrl(e.data);
             setOnRec(true);
           };
-          alert('녹음이 완료되었습니다.');
+          alert("녹음이 완료되었습니다.");
         } else {
           setOnRec(false);
         }
@@ -67,51 +60,46 @@ const Record = ({ setFile }) => {
   };
 
   const offRecAudio = () => {
-    // dataavailable 이벤트로 Blob 데이터에 대한 응답을 받을 수 있음
     media.ondataavailable = function (e) {
       console.log(e.data);
       setAudioUrl(e.data);
       setOnRec(true);
     };
 
-    // 모든 트랙에서 stop()을 호출해 오디오 스트림을 정지
     stream.getAudioTracks().forEach(function (track) {
       track.stop();
     });
 
-    // 미디어 캡처 중지
     media.stop();
-
-    // 메서드가 호출 된 노드 연결 해제
     analyser.disconnect();
     source.disconnect();
   };
 
   const onSubmitAudioFile = useCallback(() => {
     if (audioUrl) {
-      sessionStorage.setItem('url', URL.createObjectURL(audioUrl)); // 출력된 링크에서 녹음된 오디오 확인 가능
+      sessionStorage.setItem("url", URL.createObjectURL(audioUrl));
     }
-    // File 생성자를 사용해 파일로 변환
-    const sound = new File([audioUrl], 'haii-audio', {
+
+    const sound = new File([audioUrl], "haii-audio", {
       lastModified: new Date().getTime(),
-      type: 'audio',
+      type: "audio",
     });
 
-    sessionStorage.setItem('file', sound.name);
-    navigate('/');
+    sessionStorage.setItem("file", sound.name);
+    navigate("/");
   }, [audioUrl]);
   return (
     <StyledRecord>
-      <section className='wave-form'>Haii-audio</section>
-      <section className='sound-wave'>
-        <img alt='파형' src={waveSound} />
+      <section className="wave-form">Haii-audio</section>
+      <section className="sound-wave">
+        <img alt="파형" src={waveSound} />
       </section>
-      <section className='record-time'>{timer}</section>
-      <footer className='record-btn-box'>
-        <div className='record-btn' onClick={onRec ? onRecAudio : offRecAudio}>
+      <section className="record-time">{timer}</section>
+      <footer className="record-btn-box">
+        <div className="record-btn" onClick={onRec ? onRecAudio : offRecAudio}>
           {onRec ? <StyledCircle /> : <StyledSquare />}
         </div>
-        <div className='complete-btn' onClick={onSubmitAudioFile}>
+        <div className="complete-btn" onClick={onSubmitAudioFile}>
           완료
         </div>
       </footer>
